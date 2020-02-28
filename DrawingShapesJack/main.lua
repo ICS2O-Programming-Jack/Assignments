778dfa1f6441d24157580665e2b1c2be8ace87af
s445307 <jack.fiander@ocsbstudent.ca>
2020-02-28 14:03:47 -0500

    Initial commit

---
+++ DrawingShapesJack/main.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------------------
+--Jack Fiander 
+-- ICS20
+--This program displays four shapes on an ipad.
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+local myTriangle
+local verticesTriangle = {-40, -50, 40, -50, 0, 50}
+local baseOfTriangle
+local heightOfTriangle
+local areaOfTriangle
+
+display.setStatusBar(display.HiddenStatusBar)
+
+display.setDefault("background", 202/255, 0/255, 188/255)
+
+display.newPolygon(100, 100, verticesTriangle)
+
