f902b8102ed3bc463c8c5f57d7292588e619a2af
s445307 <jack.fiander@ocsbstudent.ca>
2020-03-09 14:32:03 -0400

    I updatde my code to have another moving object

--- AnimatingImagesJack/main.lua
+++ AnimatingImagesJack/main.lua
@@ -1,58 +1,75 @@
 -----------------------------------------------------------------------------------------
---
+--Jack Fiander
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
 
 -- Your code here--hide status bar
 display.setStatusBar(display.HiddenStatusBar)
-
+--sound variables 
+local correctSoundChannel
+local BoingSound = audio.loadSound( "Sounds/BoingSound.mp3")
 --Global Variables
 scrollspeed = 3
 scrollRspeed = -3
 --BG image width and height
-local backgroundImage = display.newImageRect("Images/Background.png", 2048, 1536)
+local backgroundImage = display.newImageRect("Images/Background.jpg", 1000, 1000)
 --character image width and height
 local Danny = display.newImageRect("Images/Danny.png", 200, 200)
-
+local Pewds = display.newImageRect("Images/Pewdiepie.png", 150, 150)
 --set the image to be transparent
 Danny.alpha = 0
 
---set the initial x and y position of the beetleship
+--set the initial x and y position of Danny Davito 
 Danny.x = 0
 Danny.y = display.contentHeight/3
 
---Function MoveShip
+--Function MoveDanny
 local function MoveDanny(event)
 	--add the scroll speed to the x value of the ship 
-	beetleship.x = beetleship.x + scrollspeed
+	Danny.x = Danny.x + scrollspeed
 	--Change the transparency
-	beetleship.yScale = .5
-	beetleship.xScale = .5
-	beetleship.alpha = beetleship.alpha + 0.01
+	Danny.yScale = .5
+	Danny.xScale = .5
+	Danny.alpha = Danny.alpha + 0.01
 end
 
 --repeat this over and over
-Runtime:addEventListener("enterFrame", MoveShip)
+Runtime:addEventListener("enterFrame", MoveDanny)
 
-local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
+local Corgie = display.newImageRect("Images/Corgie.png", 200, 200)
 
 --set the image to be transparent
-rocketship.alpha = 1
+Corgie.alpha = 1
 
---set the initial x and y position of the rocketship
-rocketship.x = 500
-rocketship.y = display.contentHeight*(2/3)
+--set the initial x and y position of the Corgie
+Corgie.x = 500
+Corgie.y = display.contentHeight*(2/3)
 
---Function MoveShip
+--Function MoveCorgie
 local function MoveCorgie(event)
-	--add the scroll speed to the x value of the ship 
-	rocketship.x = rocketship.x + scrollRspeed
+	--add the scroll speed to the x value of the Corgie
+	Corgie.x = Corgie.x + scrollRspeed
 	--Change the transparency
-	rocketship:scale(1.004, 1.004)
-	rocketship.alpha = rocketship.alpha - 0.00000001
+	Corgie:scale(1.004, 1.004)
+	Corgie.alpha = Corgie.alpha - 0.00000001
 
 end
 
 --repeat this over and over
 Runtime:addEventListener("enterFrame", MoveCorgie)
+
+Pewds.x = 0
+Pewds.y = 0
+-- move Pewdiepie
+local function MovePewds(event)
+	--add the scroll speed to the x value of Pewdiepie
+	Pewds.x = Pewds.x + scrollspeed 
+	Pewds.y = Pewds.y + scrollspeed
+	Pewds:rotate(10)
+	timer.performWithDelay(10000)
+end
+
+--repeat this over and over
+Runtime:addEventListener("enterFrame", MovePewds)
+correctSoundChannel = audio.play(BoingSound)
